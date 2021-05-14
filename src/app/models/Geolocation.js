08953c564646b18e6fce986ece0f4b0af67bd8ac
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2021-05-14 15:50:48 -0300

    Deleta este model

--- src/app/models/Geolocation.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    const Geolocation = sequelize.define('Geolocation', {
-        name: DataTypes.STRING,
-        geom: DataTypes.GEOMETRY
-    });
-
-    return Geolocation;
-}
